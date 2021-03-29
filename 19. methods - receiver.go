86ffe106a3dae546ba55ca40d189e59555e9312a
Adam <amalek999@o2.pl>
2021-03-30 00:50:24 +0200

    readme + receiver

--- 19. methods - receiver.go
+++ 19. methods - receiver.go
@@ -11,12 +11,20 @@ func main() {
 
 	fmt.Println("Original content", myLetter.content)
 	myLetter.changeContentValue("Content changed by value")
-	fmt.Println("After changeContentValue", myLetter.content)
+	fmt.Println("After changeContentValue:", myLetter.content)
 	myLetter.changeContentPointer("Content changed by pointer")
-	fmt.Println("After changeContentPointer", myLetter.content)
-	// when invoking method by value object gets copied
-	fmt.Println("Original object address", myLetter)
-	fmt.Println("Original object address", myLetter.showAddress())
+	fmt.Println("After changeContentPointer:", myLetter.content)
+
+	fmt.Println("When invoking method by value object gets copied")
+	fmt.Printf("Original object address %p\n", &myLetter)
+	fmt.Printf("Copied object address %p\n", myLetter.showAddress())
+
+	// When to use pointer receiver?
+	// 1. when we want to modify original objects
+	// 2. when struct is big, so we can skip copying large object
+
+	// Golang takes care of converting object to pointer
+
 }
 
 func (l letter) changeContentValue(newContent string) {
